1c29c98507aaa27f5cf96a80ccf2c9421f3d14bb
OfirDataloopAI <ofir.g@dataloop.ai>
2024-06-05 12:33:26 +0300

    in progress

--- custom_converter.py
+++ custom_converter.py
@@ -7,8 +7,11 @@ import shutil
 from zipfile import ZipFile
 from scipy.spatial.transform import Rotation
 import math
-import numpy as np
 from io import BytesIO
+import numpy as np
+import struct
+import open3d as o3d
+import pathlib
 
 from dtlpylidar.parsers.base_parser import LidarFileMappingParser
 
@@ -76,40 +79,30 @@ class FixTransformation:
 
 
 class LidarCustomParser(LidarFileMappingParser):
-    def __init__(self,
-                 enable_ir_cameras: str,
-                 enable_rgb_cameras: str,
-                 enable_rgb_highres_cameras: str):
+    def __init__(self):
         self.attributes_id_mapping_dict = None
-        # Handle Cameras Options
-        ir_cameras = ['ir_center', 'ir_left', 'ir_right']
-        rgb_cameras = ['rgb_center', 'rgb_left', 'rgb_right']
-        rgb_highres_cameras = ['rgb_highres_center', 'rgb_highres_left', 'rgb_highres_right']
 
-        self.camera_list = list()
-        if str(enable_ir_cameras) == "true" or str(enable_ir_cameras) == "True":
-            self.camera_list += ir_cameras
-        if str(enable_rgb_cameras) == "true" or str(enable_rgb_cameras) == "True":
-            self.camera_list += rgb_cameras
-        if str(enable_rgb_highres_cameras) == "true" or str(enable_rgb_highres_cameras) == "True":
-            self.camera_list += rgb_highres_cameras
-
+        # Save input paths
+        self.number_of_frames = 10
+        self.data_3d_path = os.path.join("data_3d_test_slam", "test_0")
+        self.data_2d_path = os.path.join("data_2d_test_slam", "test_0")
+        self.calibration_path = "calibration"
         super().__init__()
 
-    def attributes_id_mapping(self, dataset):
-        recipe = dataset.recipes.list()[0]
-        attributes_mapping = {}
-
-        instructions = recipe.metadata.get('system', dict()).get('script', dict()).get('entryPoints', dict()).get(
-            'annotation:context:set', dict()).get('_instructions', list())
-
-        for instruction in instructions:
-            instructions2 = instruction.get('body', dict()).get('block', dict()).get('_instructions', list())
-            for instruction2 in instructions2:
-                title = instruction2.get('title', None)
-                key = instruction2.get('body', dict()).get('key', None)
-                attributes_mapping[title] = key
-        self.attributes_id_mapping_dict = attributes_mapping
+    # def attributes_id_mapping(self, dataset):
+    #     recipe = dataset.recipes.list()[0]
+    #     attributes_mapping = {}
+    #
+    #     instructions = recipe.metadata.get('system', dict()).get('script', dict()).get('entryPoints', dict()).get(
+    #         'annotation:context:set', dict()).get('_instructions', list())
+    #
+    #     for instruction in instructions:
+    #         instructions2 = instruction.get('body', dict()).get('block', dict()).get('_instructions', list())
+    #         for instruction2 in instructions2:
+    #             title = instruction2.get('title', None)
+    #             key = instruction2.get('body', dict()).get('key', None)
+    #             attributes_mapping[title] = key
+    #     self.attributes_id_mapping_dict = attributes_mapping
 
     @staticmethod
     def extract_zip_file(zip_filepath: str):
@@ -131,41 +124,42 @@ class LidarCustomParser(LidarFileMappingParser):
         return data_path
 
     @staticmethod
-    def create_lidar_dataset(item: dl.Item, overwrite: bool):
-        dataset_name = item.name
+    def _convert_kitti_bin_to_pcd(binFilePath):
+        size_float = 4
+        list_pcd = []
+        with open(binFilePath, "rb") as f:
+            byte = f.read(size_float * 4)
+            while byte:
+                x, y, z, intensity = struct.unpack("ffff", byte)
+                list_pcd.append([x, y, z])
+                byte = f.read(size_float * 4)
+        np_pcd = np.asarray(list_pcd)
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(np_pcd)
+        return pcd
 
-        try:
-            dataset = item.project.datasets.create(dataset_name=dataset_name)
-        except:
-            if overwrite:
-                dataset = item.project.datasets.get(dataset_name=dataset_name)
-            else:
-                dataset_name += f'_{datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")}'
-                dataset = item.project.datasets.create(dataset_name=dataset_name)
+    def data_pre_processing(self, data_path: str):
+        data_3d_path = os.path.join(data_path, self.data_3d_path)
 
-        return dataset
+        bin_filepaths = pathlib.Path(data_3d_path).rglob("*.bin")
+        bin_filepaths = sorted(list(bin_filepaths))[0:self.number_of_frames]
+        for bin_filepath in bin_filepaths:
+            input_bin_filepath = str(bin_filepath)
+            pcd = self._convert_kitti_bin_to_pcd(binFilePath=input_bin_filepath)
+            output_pcd_filename = input_bin_filepath.replace(".bin", ".pcd")
+            o3d.io.write_point_cloud(filename=output_pcd_filename, pointcloud=pcd, write_ascii=True)
 
     def upload_pcds_and_images(self, data_path: str, dataset: dl.Dataset):
-        scene = None
-        dir_items = os.listdir(path=data_path)
-        for dir_item in dir_items:
-            if ".json" in dir_item:
-                try:
-                    calibration_json = os.path.join(data_path, dir_item)
-                    # scene = raillabel.load(calibration_json)
-                    break
-                except:
-                    continue
+        data_2d_path = os.path.join(data_path, self.data_2d_path)
+        data_3d_path = os.path.join(data_path, self.data_3d_path)
 
-        if scene is None:
-            dl.exceptions.NotFound("Couldn't find supported json for 'raillabel'")
+        # Get pcd filepaths
+        pcd_filepaths = pathlib.Path(data_3d_path).rglob("*.pcd")
+        pcd_filepaths = sorted(list(pcd_filepaths))
 
         # Loop through frames
-        frames = scene.frames
-        for lidar_frame, (frame_num, frame) in enumerate(frames.items()):
-            # Sensor ego pose
-            ego_pose = frame.sensors['lidar']
-            pcd_filepath = os.path.join(data_path, ego_pose.uri[1:])
+        for lidar_frame, pcd_filepath in enumerate(pcd_filepaths):
+            pcd_filepath = str(pcd_filepath)
             dataset.items.upload(
                 local_path=pcd_filepath,
                 remote_path=f"/lidar",
@@ -177,42 +171,43 @@ class LidarCustomParser(LidarFileMappingParser):
                 f"as: '/lidar/{lidar_frame}.pcd'"
             )
 
-            # Loop through images
-            for idx, image in enumerate(self.camera_list):
-                # Get sensor
-                sensor_reference = frame.sensors[image]
-                image_filepath = os.path.join(data_path, sensor_reference.uri[1:])
-                ext = os.path.splitext(p=sensor_reference.uri)[1]
-                dataset.items.upload(
-                    local_path=image_filepath,
-                    remote_path=f"/frames/{lidar_frame}",
-                    remote_name=f"{idx}{ext}",
-                    overwrite=True
-                )
-                print(
-                    f"Uploaded to 'frames' directory, the file: '{image_filepath}', "
-                    f"as: '/frames/{lidar_frame}/{idx}{ext}'"
-                )
+            # # Loop through images
+            # for idx, image in enumerate(self.camera_list):
+            #     # Get sensor
+            #     sensor_reference = frame.sensors[image]
+            #     image_filepath = os.path.join(data_path, sensor_reference.uri[1:])
+            #     ext = os.path.splitext(p=sensor_reference.uri)[1]
+            #     dataset.items.upload(
+            #         local_path=image_filepath,
+            #         remote_path=f"/frames/{lidar_frame}",
+            #         remote_name=f"{idx}{ext}",
+            #         overwrite=True
+            #     )
+            #     print(
+            #         f"Uploaded to 'frames' directory, the file: '{image_filepath}', "
+            #         f"as: '/frames/{lidar_frame}/{idx}{ext}'"
+            #     )
 
     def create_mapping_json(self, data_path: str, dataset: dl.Dataset):
-        scene = None
-        dir_items = os.listdir(path=data_path)
-        for dir_item in dir_items:
-            if ".json" in dir_item:
-                try:
-                    calibration_json = os.path.join(data_path, dir_item)
-                    # scene = raillabel.load(calibration_json)
-                    break
-                except:
-                    continue
-
-        if scene is None:
-            dl.exceptions.NotFound("Couldn't find supported json for 'raillabel'")
-
         output_frames = dict()
 
+        data_2d_path = os.path.join(data_path, self.data_2d_path)
+        data_3d_path = os.path.join(data_path, self.data_3d_path)
+
+        # Get pcd filepaths
+        pcd_filepaths = pathlib.Path(data_3d_path).rglob("*.pcd")
+        pcd_filepaths = sorted(list(pcd_filepaths))
+
+        # Get timestamps
+        timestamp_filepath = list(pathlib.Path(data_3d_path).rglob("*.txt"))[0]
+        with open(timestamp_filepath, "r") as f:
+            timestamps = f.readlines()
+            timestamps = [timestamp.strip() for timestamp in timestamps]
+
+
+        # TODO:
         # Loop through frames
-        frames = scene.frames
+        frames = []
         for lidar_frame, (frame_num, frame) in enumerate(frames.items()):
             # Sensor ego pose
             ego_pose = frame.sensors['lidar']
@@ -221,7 +216,6 @@ class LidarCustomParser(LidarFileMappingParser):
             output_frame_dict = {
                 "metadata": {
                     "frame": int(frame_num),
-                    "sensor_uri": ego_pose.uri
                 },
                 "path": f"lidar/{lidar_frame}.pcd",
                 "timestamp": float(frame.timestamp),
@@ -240,58 +234,58 @@ class LidarCustomParser(LidarFileMappingParser):
             }
 
             # Loop through images
-            for idx, image in enumerate(self.camera_list):
-                # Get sensor
-                sensor_reference = frame.sensors[image]
-
-                # Get extrinsics
-                extrinsics = sensor_reference.sensor.extrinsics
-                quaternion = np.array([extrinsics.quat.x, extrinsics.quat.y, extrinsics.quat.z, extrinsics.quat.w])
-                position = np.array([extrinsics.pos.x, extrinsics.pos.y, extrinsics.pos.z])
-
-                # Apply camera transformation fix
-                translation, rotation = FixTransformation.fix_camera_transformation(
-                    quaternion=quaternion,
-                    position=position
-                )
-
-                # Output image dict
-                ext = os.path.splitext(p=sensor_reference.uri)[1]
-                image_dict = {
-                    "metadata": {
-                        "frame": int(frame_num),
-                        "image_uri": sensor_reference.uri,
-                    },
-                    "image_path": f"frames/{lidar_frame}/{idx}{ext}",
-                    "timestamp": float(sensor_reference.timestamp),
-                    "intrinsics": {
-                        "fx": sensor_reference.sensor.intrinsics.camera_matrix[0],
-                        "fy": sensor_reference.sensor.intrinsics.camera_matrix[5],
-                        "cx": sensor_reference.sensor.intrinsics.camera_matrix[2],
-                        "cy": sensor_reference.sensor.intrinsics.camera_matrix[6],
-                    },
-                    "extrinsics": {
-                        "translation": {
-                            "x": translation["x"],
-                            "y": translation["y"],
-                            "z": translation["z"]
-                        },
-                        "rotation": {
-                            "x": rotation["x"],
-                            "y": rotation["y"],
-                            "z": rotation["z"],
-                            "w": rotation["w"]
-                        },
-                    },
-                    "distortion": {
-                        "k1": sensor_reference.sensor.intrinsics.distortion[0],
-                        "k2": sensor_reference.sensor.intrinsics.distortion[1],
-                        "k3": sensor_reference.sensor.intrinsics.distortion[2],
-                        "p1": sensor_reference.sensor.intrinsics.distortion[3],
-                        "p2": sensor_reference.sensor.intrinsics.distortion[4]
-                    }
-                }
-                output_frame_dict['images'][str(idx)] = image_dict
+            # for idx, image in enumerate(self.camera_list):
+            #     # Get sensor
+            #     sensor_reference = frame.sensors[image]
+            #
+            #     # Get extrinsics
+            #     extrinsics = sensor_reference.sensor.extrinsics
+            #     quaternion = np.array([extrinsics.quat.x, extrinsics.quat.y, extrinsics.quat.z, extrinsics.quat.w])
+            #     position = np.array([extrinsics.pos.x, extrinsics.pos.y, extrinsics.pos.z])
+            #
+            #     # Apply camera transformation fix
+            #     translation, rotation = FixTransformation.fix_camera_transformation(
+            #         quaternion=quaternion,
+            #         position=position
+            #     )
+            #
+            #     # Output image dict
+            #     ext = os.path.splitext(p=sensor_reference.uri)[1]
+            #     image_dict = {
+            #         "metadata": {
+            #             "frame": int(frame_num),
+            #             "image_uri": sensor_reference.uri,
+            #         },
+            #         "image_path": f"frames/{lidar_frame}/{idx}{ext}",
+            #         "timestamp": float(sensor_reference.timestamp),
+            #         "intrinsics": {
+            #             "fx": sensor_reference.sensor.intrinsics.camera_matrix[0],
+            #             "fy": sensor_reference.sensor.intrinsics.camera_matrix[5],
+            #             "cx": sensor_reference.sensor.intrinsics.camera_matrix[2],
+            #             "cy": sensor_reference.sensor.intrinsics.camera_matrix[6],
+            #         },
+            #         "extrinsics": {
+            #             "translation": {
+            #                 "x": translation["x"],
+            #                 "y": translation["y"],
+            #                 "z": translation["z"]
+            #             },
+            #             "rotation": {
+            #                 "x": rotation["x"],
+            #                 "y": rotation["y"],
+            #                 "z": rotation["z"],
+            #                 "w": rotation["w"]
+            #             },
+            #         },
+            #         "distortion": {
+            #             "k1": sensor_reference.sensor.intrinsics.distortion[0],
+            #             "k2": sensor_reference.sensor.intrinsics.distortion[1],
+            #             "k3": sensor_reference.sensor.intrinsics.distortion[2],
+            #             "p1": sensor_reference.sensor.intrinsics.distortion[3],
+            #             "p2": sensor_reference.sensor.intrinsics.distortion[4]
+            #         }
+            #     }
+            #     output_frame_dict['images'][str(idx)] = image_dict
 
             output_frames[str(lidar_frame)] = output_frame_dict
 
@@ -307,11 +301,12 @@ class LidarCustomParser(LidarFileMappingParser):
         data_path = self.extract_zip_file(zip_filepath=zip_filepath)
 
         try:
+            self.data_pre_processing(data_path=data_path)
             self.upload_pcds_and_images(data_path=data_path, dataset=lidar_dataset)
             mapping_item = self.create_mapping_json(data_path=data_path, dataset=lidar_dataset)
             frames_item = self.parse_data(mapping_item=mapping_item)
-            self.upload_pre_annotation_lidar(frames_item=frames_item, data_path=data_path)
-            self.upload_pre_annotation_images(frames_item=frames_item, data_path=data_path)
+            # self.upload_pre_annotation_lidar(frames_item=frames_item, data_path=data_path)
+            # self.upload_pre_annotation_images(frames_item=frames_item, data_path=data_path)
         finally:
             shutil.rmtree(path=data_path, ignore_errors=True)
 
@@ -319,25 +314,20 @@ class LidarCustomParser(LidarFileMappingParser):
 
 
 def main():
-    cp = LidarCustomParser(
-        enable_ir_cameras="false",
-        enable_rgb_cameras="false",
-        enable_rgb_highres_cameras="true"
-    )
+    cp = LidarCustomParser()
 
-    data_path = "./data"
-    dataset = dl.datasets.get(dataset_id="66099e6289c8593e33498ce1")
+    data_path = "./KITTI-360/test_data"
+    dataset = dl.datasets.get(dataset_id="66602cc51fb4fc872de5cfca")
 
-    # cp.upload_pcds_and_images(data_path=data_path, dataset=dataset)
-    mapping_item = cp.create_mapping_json(data_path=data_path, dataset=dataset)
-    frames_item = cp.parse_data(mapping_item=mapping_item)
+    # cp.data_pre_processing(data_path=data_path)
+    cp.upload_pcds_and_images(data_path=data_path, dataset=dataset)
+    # mapping_item = cp.create_mapping_json(data_path=data_path, dataset=dataset)
+    # frames_item = cp.parse_data(mapping_item=mapping_item)
     # mapping_item = dataset.items.get(item_id="65f32fa45c63d275df8dc81d")
 
     # frames_item = dataset.items.get(filepath="/frames.json")
-    cp.upload_pre_annotation_lidar(frames_item=frames_item, data_path=data_path)
+    # cp.upload_pre_annotation_lidar(frames_item=frames_item, data_path=data_path)
     # cp.upload_pre_annotation_images(frames_item=frames_item, data_path=data_path)
-
-    # cp.upload_radar_with_annotations(dataset=dataset, data_path=data_path)
 
 
 if __name__ == "__main__":
--- dataset_loader.py
+++ dataset_loader.py
@@ -14,7 +14,6 @@ class DatasetLidarKITTI(dl.BaseServiceRunner):
         dl.use_attributes_2(state=True)
 
         self.dataset_url = "https://download.data.fid-move.de/dzsf/osdar23/1_calibration_1.2.zip"
-        # self.dataset_url = "https://download.data.fid-move.de/dzsf/osdar23/9_station_ruebenkamp_9.7.zip"
         self.zip_filename = "data.zip"
 
     def _download_zip(self):
@@ -28,40 +27,36 @@ class DatasetLidarKITTI(dl.BaseServiceRunner):
         return zip_filepath
 
     def upload_dataset(self, dataset: dl.Dataset, source: str):
-        self._import_recipe_ontology(dataset=dataset)
+        # self._import_recipe_ontology(dataset=dataset)
         if self.zip_filename not in os.listdir(path=os.getcwd()):
             zip_filepath = self._download_zip()
         else:
             zip_filepath = os.path.join(os.getcwd(), self.zip_filename)
 
         item: dl.Item
-        # lidar_parser = lidar.LidarCustomParser(
-        #     enable_ir_cameras=self.enable_ir_cameras,
-        #     enable_rgb_cameras=self.enable_rgb_cameras,
-        #     enable_rgb_highres_cameras=self.enable_rgb_highres_cameras
-        # )
-        # frames_item = lidar_parser.custom_parse_data(zip_filepath=zip_filepath, lidar_dataset=dataset)
-        # return frames_item
+        lidar_parser = lidar.LidarCustomParser()
+        frames_item = lidar_parser.custom_parse_data(zip_filepath=zip_filepath, lidar_dataset=dataset)
+        return frames_item
 
 
 def test_download():
-    sr = DatasetLidarOSDAR()
+    sr = DatasetLidarKITTI()
     sr._download_zip()
 
 
-def test_import_recipe_ontology():
-    dataset_id = "66325a24241a71f884f78431"
-
-    dataset = dl.datasets.get(dataset_id=dataset_id)
-    sr = DatasetLidarOSDAR()
-    sr._import_recipe_ontology(dataset=dataset)
+# def test_import_recipe_ontology():
+#     dataset_id = "66325a24241a71f884f78431"
+#
+#     dataset = dl.datasets.get(dataset_id=dataset_id)
+#     sr = DatasetLidarKITTI()
+#     sr._import_recipe_ontology(dataset=dataset)
 
 
 def test_dataset_import():
     dataset_id = "663b93cfd03cf2f75ddeff4f"
 
     dataset = dl.datasets.get(dataset_id=dataset_id)
-    sr = DatasetLidarOSDAR()
+    sr = DatasetLidarKITTI()
     sr.upload_dataset(dataset=dataset, source="")
 
 
